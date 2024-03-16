d6e14d98ad17281b4252a424f919ad9c25435746
Jeremiah Coyle <jcoyle@gmail.com>
2024-03-16 15:57:25 -0600

    Bump minor project version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.github.paintparty/fireworks "0.1.1"
+(defproject io.github.paintparty/fireworks "0.2.0"
   :description "Color Pretty Printer for Clojure(Script)"
   :url "https://github.com/paintparty/fireworks"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
