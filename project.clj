58a109eeb716ca483f6ae87019091b0b9da1e7b0
Jeremiah Coyle <jcoyle@gmail.com>
2024-06-05 21:14:34 -0600

    Bump minor

--- project.clj
+++ project.clj
@@ -1,12 +1,13 @@
-(defproject io.github.paintparty/fireworks "0.4.0"
+(defproject io.github.paintparty/fireworks "0.5.0"
   :description "Color Pretty Printer for Clojure(Script)"
   :url "https://github.com/paintparty/fireworks"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
-  :source-paths ["src"]
+  :source-paths ["src"
+                 "../lasertag/src" ]
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [expound "0.9.0"]
-                 [io.github.paintparty/lasertag "0.4.0"]]
+                 [io.github.paintparty/lasertag "0.5.0"]]
   :repl-options {:init-ns fireworks.core}
   :deploy-repositories [["clojars" {:url           "https://clojars.org/repo"
                                     :sign-releases false}]])
