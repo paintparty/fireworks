0f02d405c239615c5ef19b9af26624a314e8c829
Jeremiah Coyle <jcoyle@gmail.com>
2024-06-09 02:58:14 -0600

    fix project file

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :source-paths ["src"
-                 "../lasertag/src" ]
+                 ;; for local dev
+                 ;;  "../lasertag/src"
+                 ]
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [expound "0.9.0"]
                  [io.github.paintparty/lasertag "0.5.0"]]
