a69e867b99b78c01470a5b92edfccbb552c43358
Jeremiah Coyle <jcoyle@gmail.com>
2024-07-06 16:41:45 -0600

    Comment

--- project.clj
+++ project.clj
@@ -9,8 +9,9 @@
                  ]
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [expound "0.9.0"]
+                ;; for testing
+                ;;  [com.taoensso/tufte "2.6.3"]
                  [io.github.paintparty/lasertag "0.5.0"]]
   :repl-options {:init-ns fireworks.core}
   :deploy-repositories [["clojars" {:url           "https://clojars.org/repo"
                                     :sign-releases false}]])
-
