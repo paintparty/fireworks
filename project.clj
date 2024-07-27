c5a1668e8a25e5f7dda9ede5ef3f1bbf2aae1037
Jeremiah Coyle <jcoyle@gmail.com>
2024-07-26 22:58:48 -0400

    snapshot version

--- project.clj
+++ project.clj
@@ -1,18 +1,18 @@
-(defproject io.github.paintparty/fireworks "0.5.0"
+(defproject io.github.paintparty/fireworks "0.6.0-SNAPSHOT"
   :description "Color Pretty Printer for Clojure(Script)"
   :url "https://github.com/paintparty/fireworks"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :source-paths ["src"
-                 "../get-rich/src/"
-                 ;; for local dev
-                 ;; "../lasertag/src"
+                ;;  for local dev
+                ;;  "../get-rich/src"
+                ;;  "../lasertag/src"
                  ]
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [expound "0.9.0"]
-                ;; for testing
+                ;;  for testing
                 ;;  [com.taoensso/tufte "2.6.3"]
-                 [io.github.paintparty/get-rich "0.1.0-a"]
+                 [io.github.paintparty/get-rich "0.2.0"]
                  [io.github.paintparty/lasertag "0.5.1"]]
   :repl-options {:init-ns fireworks.core}
   :deploy-repositories [["clojars" {:url           "https://clojars.org/repo"
