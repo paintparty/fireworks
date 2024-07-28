e2a25ffe8eab750b9d8dca2425591f5c9bedea6b
Jeremiah Coyle <jcoyle@gmail.com>
2024-07-28 19:33:41 -0400

    snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.github.paintparty/fireworks "0.6.0"
+(defproject io.github.paintparty/fireworks "0.6.0-SNAPSHOT"
   :description "Color Pretty Printer for Clojure(Script)"
   :url "https://github.com/paintparty/fireworks"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
