f0bd3fb86a9e50c8106fc1a8514f0e73848fb44d
Jeremiah Coyle <jcoyle@gmail.com>
2024-11-06 22:20:51 -0700

    Update lasertag-dep

--- project.clj
+++ project.clj
@@ -13,7 +13,7 @@
                  ;; for testing
                  ;; [com.taoensso/tufte "2.6.3"]
                  [io.github.paintparty/bling "0.2.0"]
-                 [io.github.paintparty/lasertag "0.7.0"]]
+                 [io.github.paintparty/lasertag "0.8.0"]]
   :repl-options {:init-ns fireworks.core}
   :deploy-repositories [["clojars" {:url           "https://clojars.org/repo"
                                     :sign-releases false}]])
