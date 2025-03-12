24e658c327cb7af716c9678cd69db67defa785e0
Jeremiah Coyle <jcoyle@gmail.com>
2025-03-12 18:16:24 -0400

    bump lasertag dep

--- project.clj
+++ project.clj
@@ -6,14 +6,14 @@
   :source-paths ["src"
                  ;; for local dev bling and fireworks deps
                 ;;  "../bling/src"
-                 ;; "../lasertag/src"
+                ;;  "../lasertag/src"
                  ]
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [expound "0.9.0"]
                  ;; for testing
                  ;; [com.taoensso/tufte "2.6.3"]
-                 [io.github.paintparty/bling "0.4.2"]
-                 [io.github.paintparty/lasertag "0.10.0"]]
+                 [io.github.paintparty/bling "0.5.2"]
+                 [io.github.paintparty/lasertag "0.10.1"]]
   :repl-options {:init-ns fireworks.core}
   :deploy-repositories [["clojars" {:url           "https://clojars.org/repo"
                                     :sign-releases false}]])
