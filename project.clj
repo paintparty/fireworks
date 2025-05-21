616d5d82d781b80d5e19fb92d0a5d0137771fd5a
Jeremiah Coyle <jcoyle@gmail.com>
2025-05-21 12:20:53 -0400

    Update deps and bump version

--- project.clj
+++ project.clj
@@ -1,20 +1,17 @@
-(defproject io.github.paintparty/fireworks "0.11.0"
+(defproject io.github.paintparty/fireworks "0.12.0-SNAPSHOT"
   :description "Themeable print debugging library for Clojure, ClojureScript, and Babashka"
   :url "https://github.com/paintparty/fireworks"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :source-paths ["src"
-                 ;; for local dev bling and fireworks deps
-                 "../bling/src"
-                ;;  "../lasertag/src"
+                 ;; for local dev and fireworks deps
+                 ;; "../lasertag/src"
                  ]
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [expound "0.9.0"]
                  ;; for testing
                  ;; [com.taoensso/tufte "2.6.3"]
-                 [io.github.paintparty/bling "0.6.0"]
-                 [io.github.paintparty/lasertag "0.11.1"]
-                 [doric "0.9.0"]]
+                 [io.github.paintparty/lasertag "0.11.1"]]
 
   ;; :profiles     {:1.12-storm
   ;;                {:dependencies [[com.github.flow-storm/clojure "1.12.0-9"]
