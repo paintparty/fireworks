407e3cc8b02a998cfabdf505b6ddfd4c96721067
Jeremiah Coyle <jcoyle@gmail.com>
2025-07-11 15:21:28 -0700

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.github.paintparty/fireworks "0.12.5"
+(defproject io.github.paintparty/fireworks "0.12.6"
   :description "Themeable print debugging library for Clojure, ClojureScript, and Babashka"
   :url "https://github.com/paintparty/fireworks"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
@@ -11,7 +11,7 @@
                  [expound "0.9.0"]
                  ;; for testing
                  ;; [com.taoensso/tufte "2.6.3"]
-                 [io.github.paintparty/lasertag "0.11.3"]]
+                 [io.github.paintparty/lasertag "0.11.5"]]
   :repl-options {:init-ns fireworks.core}
   :deploy-repositories [["clojars" {:url           "https://clojars.org/repo"
                                     :sign-releases false}]])
