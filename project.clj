42b67e0e97bb31382a0f6472daf67f5cab9f217d
Jeremiah Coyle <jcoyle@gmail.com>
2026-06-29 17:11:59 -0600

    Update project file

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :source-paths ["src"
                  ;; for local dev
-                 "../lasertag/src"
+                ;;  "../lasertag/src"
                  ]
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [expound "0.9.0"]
@@ -13,7 +13,7 @@
   :repl-options {:init-ns fireworks.core}
 
   ;; rewrite-clj is dev/test-only: it backs the experimental golden-snapshotter
-  ;; in fireworks.test-util (snapshot!). Not a runtime dependency of the library.
+  ;; in fireworks.test-util/snapshot!. Not a runtime dependency of the library.
   :profiles {:dev {:dependencies [[rewrite-clj "1.1.47"]]}}
 
   :deploy-repositories [["clojars" {:url           "https://clojars.org/repo"
